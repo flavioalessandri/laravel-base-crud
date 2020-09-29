988476d53c8dc59c4a31e6ca8e339dde81dd3900
Flavio Alessandri <flavio.alessandri@gmail.com>
2020-09-29 17:07:05 +0200

    DBhotel - card with display flex

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -2,16 +2,47 @@
 
 @section('content')
 
-<section id="recensioni">
+<section id="post">
 
-<table>
+  {{-- <h2>Posts</h2> --}}
+
+
+  @foreach ($recensioni as $recensione)
+
+  <div class="post">
+
+      <h3>ID: {{$recensione->id}}</h3>
+
+
+      <h3>Titolo</h3>
+      <span>{{$recensione->title}}</span>
+
+      <h3>Testo</h3>
+      <p>{{$recensione->text}}</p>
+
+      <h3>Categoria</h3>
+      <p>{{$recensione->category}}</p>
+
+      <h3>Like <i class="far fa-thumbs-up"></i></h3>
+      <p>{{$recensione->like}}</p>
+
+      <h3>DisLike <i class="far fa-thumbs-down"></i></h3>
+      <p>{{$recensione->dislike}}</p>
+
+    </div>
+    @endforeach
+
+
+
+
+{{-- <table>
   <caption>Reviews</caption>
   <tr>
     <th>Titolo</th>
     <th>Testo</th>
     <th>Categoria</th>
-    <th>Like</th>
-    <th>DisLike</th>
+    <th>Like <i class="far fa-thumbs-up"></i></th>
+    <th>DisLike <i class="far fa-thumbs-down"></i></th>
   </tr>
 
   @foreach ($recensioni as $recensione)
@@ -22,15 +53,15 @@
       <td>{{$recensione->category}}</td>
       <td>{{$recensione->like}}</td>
       <td>{{$recensione->dislike}}</td>
-    </tr>
+    </tr> --}}
 
-
+{{--
   @endforeach
 
 
 
 
-</table>
+</table> --}}
 
 
 </section>
